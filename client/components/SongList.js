6613ee065bc3fc87ede84471289e4ff0d639c4b4
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-04-15 18:06:20 -0500

    Added SongList component.

---
+++ client/components/SongList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class SongsList extends Component {
+  render() {
+    return (
+      <div>
+        SongsList
+      </div>
+    )
+  }
+}
